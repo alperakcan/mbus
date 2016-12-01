3629ff895583e0cf47a5ad21227772728051ca6d
Alper Akcan <alper.akcan@gmail.com>
2016-12-01 13:16:07 +0300

    nodejs

--- src/client/client.js
+++ src/client/client.js
@@ -1,12 +1,30 @@
 
-'use strict';
-
 const WebSocket = require('ws');
 
 function MBusClient (name, options) {
 	if (this instanceof MBusClient == false) {
 		return new MBusClient(name, options);
 	}
+	
 	this.type = 'MBusClient';
+	
 	this._socket = new WebSocket("ws://127.0.0.1:9000", "mbus");
+	
+	this._socket.on('open', function open() {
+		console.log('open');
+	})
+
+	this._socket.on('close', function close(code, message) {
+		console.log('close, code: ' + code + ', message: ' + message);
+	})
+
+	this._socket.on('message', function message(data, flags) {
+		console.log('message' + data + flags);
+	})
+
+	this._socket.on('error', function error(error) {
+		console.log('error: ' + error);
+	})
 }
+
+module.exports = MBusClient
--- test/websocket/websocket.js
+++ test/websocket/websocket.js
@@ -1,21 +1,4 @@
 
-var WebSocket = require('ws');
+var MBusClient = require('../../src/client/client.js');
 
-
-var ws = new WebSocket("ws://127.0.0.1:9000", "mbus"); 
-
-ws.on('open', function open() {
-	console.log('open');
-})
-
-ws.on('close', function close(code, message) {
-	console.log('close, code: ' + code + ', message: ' + message);
-})
-
-ws.on('message', function message(data, flags) {
-	console.log('message' + data + flags);
-})
-
-ws.on('error', function error(error) {
-	console.log('error: ' + error);
-})
+var mbc = new MBusClient('test'); 
