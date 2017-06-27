a91f4063088fa77c74b9f347120122e1031e65ba
Alper Akcan <alper.akcan@gmail.com>
2017-06-27 12:26:32 +0300

    linux

--- src/debug/debug.c
+++ src/debug/debug.c
@@ -26,10 +26,12 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
+#include <time.h>
 #include <sys/time.h>
 
 #include "debug.h"
