250898bd4939fa475e9b78170fca882dc773cd31
Alper Akcan <alper.akcan@gmail.com>
2017-07-04 01:15:15 +0300

    version

---
+++ app/version/version.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+#include "mbus/version.h"
+
+int main (int argc, char *argv[])
+{
+	(void) argc;
+	(void) argv;
+	fprintf(stdout, "mbus version\n");
+	fprintf(stdout, "  git-commit  : %s\n", mbus_git_commit());
+	fprintf(stdout, "  git-revision: %s\n", mbus_git_revision());
+}
