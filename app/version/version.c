34ec542b3bd65066c98606aa1ddfb80926a2c60d
Alper Akcan <alper.akcan@gmail.com>
2017-07-04 04:37:44 +0300

    fix build

--- app/version/version.c
+++ app/version/version.c
@@ -9,4 +9,5 @@ int main (int argc, char *argv[])
 	fprintf(stdout, "mbus version\n");
 	fprintf(stdout, "  git-commit  : %s\n", mbus_git_commit());
 	fprintf(stdout, "  git-revision: %s\n", mbus_git_revision());
+	return 0;
 }
