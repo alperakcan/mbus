8ee0a7b8f3bd727f64b90825acae01fdac0726da
Alper Akcan <alper.akcan@gmail.com>
2019-10-16 12:53:55 +0300

    fix: uninitialized byte

--- src/socket/socket.c
+++ src/socket/socket.c
@@ -544,9 +544,10 @@ bail:	mbus_socket_destroy(s);
 
 char * mbus_socket_fd_get_address (int fd, char *buffer, int length)
 {
+        socklen_t addrlen;
 	struct sockaddr_storage addr;
-	socklen_t addrlen;
-	addrlen = sizeof(addr);
+	memset(&addr, 0, sizeof(struct sockaddr_storage));
+	addrlen = sizeof(struct sockaddr_storage);
 	if (getpeername(fd, (struct sockaddr *) &addr, &addrlen) == 0) {
 		if (addr.ss_family == AF_INET) {
 			if (inet_ntop(AF_INET, &((struct sockaddr_in *) &addr)->sin_addr.s_addr, buffer, length)) {
